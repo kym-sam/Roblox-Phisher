88511b281d32fc3bdf565e60d3b19955cc5a01a0
kym-sam <kymmary50@gmail.com>
2024-07-09 23:17:40 -0300

    server

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const CFonts = require('cfonts');
 const { exec } = require('child_process');
 const app = express();
-const net = require('net');
 const port = 8080;
 
 app.use(bodyParser.json());
@@ -34,7 +33,7 @@ CFonts.say('Roblox', {
     maxLength: '0',     
 });
 
-// Função para verificar se o servidor local está ativo
+
 function checkLocalServer(url) {
     return new Promise((resolve, reject) => {
         const http = require('http');
@@ -47,21 +46,9 @@ function checkLocalServer(url) {
 }
 
 const send_url = process.argv[2]; 
-const net_server = `${send_url}`; // Substitua pela URL que deseja enviar
-
-const client = new net.Socket();
-client.connect(8080, 'localhost', () => {
-    console.log('Conectado ao servidor');
-    client.write(`[-] URL: ${send_url}`);
-    console.log(`URL recebida do shell: ${send_url}`);
-    client.end(); // Encerra a conexão após enviar os dados
-});
-
-client.on('close', () => {
-    console.log('Conexão fechada');
-});
 
 app.listen(port, () => {
     console.log('Server connected!')
     console.log(`Server is running on http://localhost:${port}`);
+    console.log(`URL recebida do shell: ${send_url}`);
 });
